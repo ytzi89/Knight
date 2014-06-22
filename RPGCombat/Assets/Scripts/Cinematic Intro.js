2d7e9fe8d28c212860d32bf0b40f5435abc49343
joebeareggo <aj_odero@yahoo.com>
2014-06-22 05:59:32 -0700

    Intro+Terrain
    Added in terrain, then buildings then worked on Intro cinematic. TODO
    the tracking + transition to the main camera->home scene. Delete most
    buildings from home.unity file and characters from the intro.scene to
    save resources. Fix c

---
+++ RPGCombat/Assets/Scripts/Cinematic Intro.js
@@ -0,0 +1,54 @@
+﻿#pragma strict
+private var waypoints : GameObject[];
+var maxSize: int = 12;
+private var currentIndex : int;
+private var currentWaypoint : Transform;
+var introCamera : Transform;
+var done : boolean = false;
+var thingToLookAt : Transform;
+
+function Start () {
+	currentIndex = 0;
+	waypoints = new GameObject[maxSize];
+	for (var child : Transform in transform)
+	{
+		if (currentIndex < maxSize)
+		{
+		waypoints[currentIndex] = child.gameObject;
+		++currentIndex;
+		}
+	}
+	currentWaypoint = GetNextWaypoint();
+}
+
+function Update () {
+	if (!done)
+	{
+		if (currentWaypoint != null)
+		{
+			if (Vector3.Distance(introCamera.position, currentWaypoint.position) < .5)
+			{
+				currentWaypoint = GetNextWaypoint();
+			}
+			else
+			{
+				introCamera.position = Vector3.Lerp(introCamera.position, currentWaypoint.position, Time.deltaTime * 2);
+				introCamera.transform.LookAt(thingToLookAt);
+			}
+		}
+		else
+			done = true;
+	}
+}
+
+function GetNextWaypoint()
+{
+	if (currentIndex > 0)
+	{
+		var nextWaypoint : Transform = waypoints[currentIndex - 1].transform;
+		--currentIndex;
+		return nextWaypoint;
+	}
+	else
+		return null;
+}
